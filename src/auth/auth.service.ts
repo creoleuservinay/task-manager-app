9c039129153500fb5d80db9d803ef0241857682b
VINAY KAITHWAS <vinay.kaithwas@expresstechsoftwares.com>
2022-01-27 11:22:03 +0530

    [Improvement]-Refactoring

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,25 +7,26 @@ import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        @InjectRepository(UsersRepository)
-        private userRepository: UsersRepository,
-        private jwtService: JwtService,
-    ) {
-    }
-    async signUp(authCredentialdto: AuthCredentialDto): Promise<void> {
-        return this.userRepository.createUser(authCredentialdto);
-    }
+  constructor(
+    @InjectRepository(UsersRepository)
+    private userRepository: UsersRepository,
+    private jwtService: JwtService,
+  ) {}
+  async signUp(authCredentialdto: AuthCredentialDto): Promise<void> {
+    return this.userRepository.createUser(authCredentialdto);
+  }
 
-    async singIn(authCredentialDto: AuthCredentialDto): Promise<{ accessToken: string }> {
-        const { username, password } = authCredentialDto;
-        const user = await this.userRepository.findOne({ username });
-        if (user && (await bcrypt.compare(password, user.password))) {
-            const payload = { username };
-            const accessToken: string = await this.jwtService.sign(payload);
-            return { accessToken };
-        } else {
-            throw new UnauthorizedException('Please check your login credentials');
-        }
+  async singIn(
+    authCredentialDto: AuthCredentialDto,
+  ): Promise<{ accessToken: string }> {
+    const { username, password } = authCredentialDto;
+    const user = await this.userRepository.findOne({ username });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payload = { username };
+      const accessToken: string = await this.jwtService.sign(payload);
+      return { accessToken };
+    } else {
+      throw new UnauthorizedException('Please check your login credentials');
     }
+  }
 }
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,24 +1,27 @@
-import { ConflictException, InternalServerErrorException } from "@nestjs/common";
-import { EntityRepository, Repository } from "typeorm";
-import { AuthCredentialDto } from "./dto/user-credentials.dto";
-import { User } from "./user.entity";
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialDto } from './dto/user-credentials.dto';
+import { User } from './user.entity';
 import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
-export class UsersRepository extends Repository<User>{
-    async createUser(authCredentialdto: AuthCredentialDto): Promise<void>{
-        const { username, password } = authCredentialdto;
-        const salt = await bcrypt.genSalt();
-        const hashedPassword = await bcrypt.hash(password, salt);
-        const user = this.create({username, password: hashedPassword});
-        try {
-            await this.save(user);           
-        } catch (error) {
-            if (error.errno == 1062) {
-                throw new ConflictException('Username already exists')
-            } else {
-                throw new InternalServerErrorException();
-            }
-        }
+export class UsersRepository extends Repository<User> {
+  async createUser(authCredentialdto: AuthCredentialDto): Promise<void> {
+    const { username, password } = authCredentialdto;
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = this.create({ username, password: hashedPassword });
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.errno == 1062) {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
     }
+  }
 }
